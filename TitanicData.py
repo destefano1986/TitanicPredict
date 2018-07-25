a90dfe527b8db9281393bdcecd3d784f5b21b5ca
destefano1986 <destefano@sina.cn>
2018-07-25 08:57:50 +0800

    Modify Factoring

--- TitanicData.py
+++ TitanicData.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import warnings
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
 
 warnings.filterwarnings('ignore')
 
@@ -177,6 +178,93 @@ def fare_binning(df):
     # Divideallfaresintoquartiles
     df['Fare_bin'] = pd.qcut(df['Fare'], 5)
     print (df['Fare_bin'].head())
+    # 在将数据分箱处理后，要么将数据factorize化，要么dummies化。
+    # factorize
+    df['Fare_bin_id'] = pd.factorize(df['Fare_bin'])[0]
+    # dummies
+    fare_bin_dummies_df = pd.get_dummies(df['Fare_bin']).rename(columns=lambda x:'Fare_' + str(x))
+    train_data = pd.concat([df, fare_bin_dummies_df], axis=1)
+
+def factoring():
+    #在进行特征工程的时候，不仅需要对训练数据进行处理，还需要同时将测试数据同训练数据一起处理，使得二者具有相同的数据类型和数据分布。
+    train_df_org = pd.read_csv('train.csv')
+    test_df_org = pd.read_csv('test.csv')
+    test_df_org['Survived'] = 0
+    combined_train_test = train_df_org.append(test_df_org)
+    PassengerId = test_df_org['PassengerId']
+
+    # 对数据进行特征工程，也就是从各项参数中提取出对输出结果有或大或小的影响的特征，将这些特征作为训练模型的依据。一般来说，我们会先从含有缺失值的特征开始。
+    # 因为“Embarked”字段的缺失值不多，所以这里我们以众数来填充：
+    combined_train_test['Embarked'].fillna(combined_train_test['Embarked'].mode().iloc[0], inplace=True)
+    # 为了后面的特征分析，这里我们将Embarked特征进行facrorizing
+    combined_train_test['Embarked'] = pd.factorize(combined_train_test['Embarked'])[0]
+    # 使用pd.get_dummies获取one-hot编码
+    emb_dummies_df = pd.get_dummies(combined_train_test['Embarked'],\
+                                    prefix=combined_train_test[['Embarked']].columns[0])
+    combined_train_test = pd.concat([combined_train_test, emb_dummies_df], axis=1)
+
+    # 对Sex也进行one-hot编码，也就是dummy处理：
+    # 为了后面的特征分析，这里我们也将Sex特征进行facrorizing
+    combined_train_test['Sex'] = pd.factorize(combined_train_test['Sex'])[0]
+    sex_dummies_df = pd.get_dummies(combined_train_test['Sex'], prefix=combined_train_test[['Sex']].columns[0])
+    combined_train_test = pd.concat([combined_train_test, sex_dummies_df], axis=1)
+
+    # Name字段，首先先从名字中提取各种称呼：
+    #combined_train_test['Title']=combined_train_test['Name'].map(lambda x:re.compile(",(. *?)\.").findall(x)[0])
+    combined_train_test['Title'] = list(map(lambda x: re.findall(r', (.*?). ', x), combined_train_test['Name']))
+    # 将各式称呼进行统一化处理
+    title_Dict = {}
+    title_Dict.update(dict.fromkeys(['Capt', 'Col', 'Major', 'Dr', 'Rev'], 'Officer'))
+    title_Dict.update(dict.fromkeys(['Don', 'Sir', 'theCountess', 'Dona', 'Lady'], 'Royalty'))
+    title_Dict.update(dict.fromkeys(['Mme', 'Ms', 'Mrs'], 'Mrs'))
+    title_Dict.update(dict.fromkeys(['Mlle', 'Miss'], 'Miss'))
+    title_Dict.update(dict.fromkeys(['Mr'], 'Mr'))
+    title_Dict.update(dict.fromkeys(['Master', 'Jonkheer'], 'Master'))
+    combined_train_test['Title'] = list(map(title_Dict, combined_train_test['Title']))
+    print (combined_train_test['Title'])
+    # 使用dummy对不同的称呼进行分列
+    # 为了后面的特征分析，这里我们也将Title特征进行facrorizing
+    combined_train_test['Title'] = pd.factorize(combined_train_test['Title'])[0]
+    title_dummies_df = pd.get_dummies(combined_train_test['Title'], prefix=combined_train_test[['Title']].columns[0])
+    combined_train_test = pd.concat([combined_train_test, title_dummies_df], axis=1)
+    # 增加名字长度的特征：
+    combined_train_test['Name_length'] = combined_train_test['Name'].apply(len)
+    # Fare字段，由前面分析可以知道，Fare项在测试数据中缺少一个值，所以需要对该值进行填充。
+    # 我们按照一二三等舱各自的均价来填充：
+    # 下面transform将函数np.mean应用到各个group中。
+    combined_train_test['Fare']=combined_train_test[['Fare']].fillna(combined_train_test.groupby('Pclass').transform(np.mean))
+    # 通过对Ticket数据的分析，我们可以看到部分票号数据有重复，同时结合亲属人数及名字的数据，和票价船舱等级对比，
+    # 我们可以知道购买的票中有家庭票和团体票，所以我们需要将团体票的票价分配到每个人的头上。
+    combined_train_test['Group_Ticket']=\
+    combined_train_test['Fare'].groupby(by=combined_train_test['Ticket']).transform('count')
+    combined_train_test['Fare'] = combined_train_test['Fare'] / combined_train_test['Group_Ticket']
+    combined_train_test.drop(['Group_Ticket'], axis=1, inplace=True)
+    # 使用binning给票价分等级
+    combined_train_test['Fare_bin'] = pd.qcut(combined_train_test['Fare'], 5)
+    # 对于5个等级的票价我们也可以继续使用dummy为票价等级分列：
+    combined_train_test['Fare_bin_id'] = pd.factorize(combined_train_test['Fare_bin'])[0]
+    fare_bin_dummies_df = pd.get_dummies(combined_train_test['Fare_bin_id']).rename(columns=lambda x:'Fare_' + str(x))
+    combined_train_test = pd.concat([combined_train_test, fare_bin_dummies_df], axis=1)
+    combined_train_test.drop(['Fare_bin'], axis=1, inplace=True)
+
+def pclass_fare_category(df, pclass1_mean_fare, pclass2_mean_fare, pclass3_mean_fare):
+    if df['Pclass']==1:
+        if df['Fare']<=pclass1_mean_fare:
+            return'Pclass1_Low'
+        else:
+            return'Pclass1_High'
+    elif df['Pclass']==2:
+        if df['Fare']<=pclass2_mean_fare:
+            return'Pclass2_Low'
+        else:
+            return'Pclass2_High'
+    elif df['Pclass']==3:
+        if df['Fare']<=pclass3_mean_fare:
+            return'Pclass3_Low'
+        else:
+            return'Pclass3_High'
+
+
 
 
 if __name__ == '__main__':
@@ -212,10 +300,24 @@ if __name__ == '__main__':
     age_scaling(train_data)
     fare_binning(train_data)
     '''
-    #P.22
-
-
-
+    factoring()
+    '''
+    # 建立PClassFareCategory
+    Pclass1_mean_fare=combined_train_test['Fare'].groupby(by=combined_train_test['Pclass']).mean().get([1]).values[0]
+    Pclass2_mean_fare=combined_train_test['Fare'].groupby(by=combined_train_test['Pclass']).mean().get([2]).values[0]
+    Pclass3_mean_fare=combined_train_test['Fare'].groupby(by=combined_train_test['Pclass']).mean().get([3]).values[0]
+    print (Pclass1_mean_fare, Pclass2_mean_fare, Pclass3_mean_fare)
+    # 建立Pclass_FareCategory
+    combined_train_test['Pclass_Fare_Category'] = combined_train_test.apply(pclass_fare_category, args=(Pclass1_mean_fare, Pclass2_mean_fare, Pclass3_mean_fare), axis=1)
+    pclass_level = LabelEncoder()
+    # 给每一项添加标签
+    pclass_level.fit(np.array(['Pclass1_Low', 'Pclass1_High', 'Pclass2_Low', 'Pclass2_High', 'Pclass3_Low', 'Pclass3_High']))
+    # 转换成数值
+    combined_train_test['Pclass_Fare_Category'] = pclass_level.transform(combined_train_test['Pclass_Fare_Category'])
+    # dummy转换
+    pclass_dummies_df = pd.get_dummies(combined_train_test['Pclass_Fare_Category']).rename(columns=lambda x: 'Pclass_' + str(x))
+    combined_train_test = pd.concat([combined_train_test, pclass_dummies_df], axis=1)
+    '''
 
 
 
